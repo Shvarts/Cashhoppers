853318519a83714bcf343e2889f1de0204444b65
viktor <viktor@viktor-System-Product-Name.(none)>
2013-06-24 18:32:30 +0300

    add_field to registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130619154955) do
+ActiveRecord::Schema.define(:version => 20130624134710) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(:version => 20130619154955) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "authentication_token"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "zip"
+    t.string   "user_name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
