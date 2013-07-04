ca4f5bff52c7f44dc80554fe3c7241f58948d0e5
victordanch <viktor.danch@gmail.com>
2013-07-04 13:00:39 +0300

    update daily hop and hop

--- app/controllers/admin/hop_ads_controller.rb
+++ app/controllers/admin/hop_ads_controller.rb
@@ -16,7 +16,7 @@ class Admin::HopAdsController < Admin::AdminController
     @hop_ad = HopAd.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html # show.html.haml
       format.json { render json: @hop_ad }
     end
   end
