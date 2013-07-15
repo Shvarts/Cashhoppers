d8a9343df0981b6e03713ea7638520a6e375dae8
victordanch <viktor.danch@gmail.com>
2013-07-15 14:37:41 +0300

    update page

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :original => "400x400>", :small => "50x50" },
                     :url  => "/images/avatars/users/:id/:style/USER_AVATAR.:extension",
-                    :default_url => "/assets/noavatar.gif",
+                    :default_url => "/images/noavatar.jpeg",
                     :path => ":rails_root/public/images/avatars/users/:id/:style/USER_AVATAR.:extension"
 
 
