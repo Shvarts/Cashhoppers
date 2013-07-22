fefab8cc986ea16051e5ee055f5c1ed890862ff2
mskubenich <misha.skubenich@gmail.com>
2013-07-22 10:31:13 +0300

    merge routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,38 +4,7 @@ CashHoppers::Application.routes.draw do
   root :to => 'pages#home'
 
   namespace :admin do
-<<<<<<< HEAD
-       get 'pages/index', :as => 'index'
-      resources :applications
-      resources :hops
-      resources :hop_tasks
-      resources :ads
-      match "hoppers/find_hopper"
-      match "hoppers/hopper_list"
-      match '/hoppers/search_hoppers' => "hoppers#search_hoppers"
-      match '/hoppers/search_hopper' => "hoppers#search_hopper"
-      match '/messages/message_history' => "messages#message_history"
-      match '/messages/show' => "messages#show"
-      delete '/messages/destroy' => "messages#destroy"
-      post '/messages/create_email' => "messages#create_email"
-      post '/messages/create_message' => "messages#create_message"
-      match '/messages/email_history' => "messages#email_history"
-      match '/messages/send_email' => "messages#send_email"
-      get '/messages/close_grid' => "messages#close_grid"
-      match '/messages/wice_grid' => "messages#wice_grid"
-      match '/messages/find_users' => "messages#find_users"
-      match '/messages/email_tool' => "messages#email_tool"
-      match '/messages/message_tool' => "messages#message_tool"
-      match '/messages/find_hop' => "messages#find_hop"
-       match '/messages/find_zip' => "messages#find_zip"
-      match '/messages/text_tool' => "messages#text_tool"
-      match '/search/current_hops' => "search#current_hops"
-      match '/search/hops_archive' => "search#hops_archive"
 
-      post 'hops/close/:id', to: 'hops#close' , :as=>'close'
-  end
-
-=======
     get 'pages/index', :as => 'index'
     resources :applications
 
@@ -57,6 +26,7 @@ CashHoppers::Application.routes.draw do
     resources :hop_tasks, only: [:new, :edit, :create, :update, :destroy, :index]
 
     resources :ads
+
     match "hoppers/find_hopper"
     match "hoppers/hopper_list"
     match '/hoppers/search_hoppers' => "hoppers#search_hoppers"
@@ -68,11 +38,14 @@ CashHoppers::Application.routes.draw do
     post '/messages/create_message' => "messages#create_message"
     match '/messages/email_history' => "messages#email_history"
     match '/messages/send_email' => "messages#send_email"
-    post '/messages/close_grid' => "messages#close_grid"
+    get '/messages/close_grid' => "messages#close_grid"
     match '/messages/wice_grid' => "messages#wice_grid"
+    match '/messages/find_users' => "messages#find_users"
     match '/messages/email_tool' => "messages#email_tool"
     match '/messages/message_tool' => "messages#message_tool"
->>>>>>> 02c2dd9bafde8e92814a047120b2bc89b79ebd61
+    match '/messages/find_hop' => "messages#find_hop"
+    match '/messages/find_zip' => "messages#find_zip"
+    match '/messages/text_tool' => "messages#text_tool"
 
   end
 
