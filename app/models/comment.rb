3a9c0e279bbd4f469758f99e7baad4d5925c51c3
mskubenich <misha.skubenich@gmail.com>
2013-07-23 19:10:02 +0300

    comment validate lenght maximum 140 symbols

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   attr_accessible :commentable_id, :text, :user_id
-  validates :text, presence: true
+  validates :text, presence: true, length: {maximum: 140}
 end
