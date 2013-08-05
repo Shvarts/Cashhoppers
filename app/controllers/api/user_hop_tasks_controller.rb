2dbe5ca201730ce4fd09d0d439b133646fb6ef7a
mskubenich <misha.skubenich@gmail.com>
2013-08-05 16:42:07 +0300

    fix api like crash

--- app/controllers/api/user_hop_tasks_controller.rb
+++ app/controllers/api/user_hop_tasks_controller.rb
@@ -51,7 +51,7 @@ class Api::UserHopTasksController < Api::ApplicationController
     unless Like.where(target_object_id: @user_hop_task.id, target_object: 'UserHopTask', user_id: @current_user.id).first
       @like = Like.create(target_object_id: @user_hop_task.id, target_object: 'UserHopTask', user_id: @current_user.id)
       Notification.create(user_id: @user_hop_task.user_id, like_id: @like.id, event_type: 'Like')
-      unless @user_hop_task.hop
+      unless @user_hop_task.hop_task.hop
         @user_hop_task.hop_task.hop.increase_score @current_user, 1
         respond_to do |format|
           format.json{
