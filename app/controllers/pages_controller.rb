59eac6805ba53f8dc54b6c760a9b888075d172d8
victordanch <viktor.danch@gmail.com>
2013-08-05 15:06:08 +0300

    add courusel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,30 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def home
+
+    #@tasks =  UserHopTask.all.order("created_at DESC")
+    collection = ['btn_login_twitter.png','facebook.png','google.png','logo.png' ]
+    @pac =[]
+    n= 0
+    for i in collection
+      n = n+1
+      if n+1 >   collection.length
+        @pac << [i , collection[0],  collection[1], collection[2]]
+
+      elsif n+2 > collection.length
+        @pac << [i , collection[n], collection[0],collection[1] ]
+
+      elsif n+3 > collection.length
+        @pac << [i , collection[n], collection[n+1],collection[0] ]
+
+      else
+        @pac << [i ,collection[n], collection[n +1],collection[n+2]]
+      end
+    end
+
+
+    @tasks
+
     if  flash[:notice]== "You updated your account successfully."
       redirect_to user_path(current_user.id)
       flash[:notice] = flash[:notice]
