58ad3977a44ddef1aeb749b3400b8c98b340606c
mskubenich <misha.skubenich@gmail.com>
2013-08-12 14:32:38 +0300

    ios push notification certificate

--- config/initializers/device_notifications.rb
+++ config/initializers/device_notifications.rb
@@ -1,6 +1,6 @@
 CashHoppers::Application::IOS_PUSHER = Grocer.pusher(
- # certificate: "/path/to/cert.pem",      # required
-  passphrase:  "",                       # optional
+  certificate: "#{Rails.root}/app/assets/certificates/ck.pem",      # required
+  passphrase:  "123456",                 # optional
   gateway:     "gateway.push.apple.com", # optional; See note below.
   port:        2195,                     # optional
   retries:     3                         # optional
