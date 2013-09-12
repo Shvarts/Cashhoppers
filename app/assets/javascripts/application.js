a26449241dcff6a4ba1a91efedeb2aa6b898309e
victordanch <viktor.danch@gmail.com>
2013-09-12 15:17:27 +0300

    update unsubscribe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -157,3 +157,28 @@ function service_menu() {
 
 
 }
+
+
+function unsubscribe_user_profile(sender){
+
+
+        $.ajax({
+            url: "",
+            data: { user_id: $(sender).attr('user_id'), subscribe: $(sender).attr('subscribe')},
+            type: 'GET',
+            beforeSend: function () {
+            },
+            complete: function(){
+            },
+            error: function(err){
+                alert("error");
+            },
+            success: function(data){
+
+                $('#subscribe').html(data);
+            }
+        });
+
+
+};
+
